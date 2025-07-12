616fa95fbeaee6c3d48b154fcfa0350d69b67a89
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-11 20:51:40 -0400

    first commit

---
+++ src/test/java/com/sbravoc/products/management/ProductsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbravoc.products.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
