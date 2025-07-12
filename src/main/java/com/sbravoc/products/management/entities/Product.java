01ace35396d0354325267121e29585b82732bc78
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-12 16:43:05 -0400

    se agrego validacion en name con anotaciones

--- src/main/java/com/sbravoc/products/management/entities/Product.java
+++ src/main/java/com/sbravoc/products/management/entities/Product.java
@@ -1,6 +1,8 @@
 package com.sbravoc.products.management.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +20,9 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "El nombre es requerido")
+    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
+    @Column(nullable = false, length = 100)
     private String name;
 
     private Integer price;
