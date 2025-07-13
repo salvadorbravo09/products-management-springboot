301798792b41fac4e056fc39db93671c091a54b2
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-13 16:25:08 -0400

    agregar método findByName en ProductRepository para búsqueda de productos por nombre

--- src/main/java/com/sbravoc/products/management/repositories/ProductRepository.java
+++ src/main/java/com/sbravoc/products/management/repositories/ProductRepository.java
@@ -3,5 +3,8 @@ package com.sbravoc.products.management.repositories;
 import com.sbravoc.products.management.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByName(String name);
 }
