d3f7bb61740864451ff204707c95d2e3396c920d
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-13 16:20:02 -0400

    agregar excepción personalizada InvalidProductDataException

---
+++ src/main/java/com/sbravoc/products/management/exceptions/InvalidProductDataException.java
@@ -0,0 +1,7 @@
+package com.sbravoc.products.management.exceptions;
+
+public class InvalidProductDataException extends RuntimeException {
+    public InvalidProductDataException(String message) {
+        super(message);
+    }
+}
