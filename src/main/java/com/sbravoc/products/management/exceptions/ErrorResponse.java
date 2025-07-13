c98f86d91632fae6a02940286df76d334658d05d
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-13 15:49:55 -0400

    agregar clase ErrorResponse para manejo de errores

---
+++ src/main/java/com/sbravoc/products/management/exceptions/ErrorResponse.java
@@ -0,0 +1,43 @@
+package com.sbravoc.products.management.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private String message;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    private String path;
+    private Map<String, String> validationsErrors;
+
+    // Constructor para errores simples
+    public ErrorResponse(int status, String error, String message, LocalDateTime timestamp) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    // Constructor para errores con path
+    public ErrorResponse(int status, String error, String message, LocalDateTime timestamp, String path) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.timestamp = timestamp;
+        this.path = path;
+    }
+}
