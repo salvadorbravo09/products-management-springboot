a92d1c4cde919c1bc3afe2f69f0c4c30ccc8d058
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-13 20:04:09 -0400

    agregar ProductCreateDTO para la creación de productos con validaciones

---
+++ src/main/java/com/sbravoc/products/management/dtos/ProductCreateDTO.java
@@ -0,0 +1,28 @@
+package com.sbravoc.products.management.dtos;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCreateDTO {
+
+    @NotBlank(message = "El nombre es requerido")
+    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
+    private String name;
+
+    @NotNull(message = "El precio es requerido")
+    @DecimalMin(value = "0.01", message = "El precio debe ser mayor a 0")
+    @Digits(integer = 10, fraction = 2, message = "El precio debe tener máximo 10 digitos enteros y 2 decimales")
+    private BigDecimal price;
+
+    @Size(max = 500, message = "La descripcion no puede exceder 500 caracteres")
+    private String description;
+}
