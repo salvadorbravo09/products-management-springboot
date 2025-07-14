4ee2d923d2dbc1bc3c96b6dd3dd3c00e0c01b043
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-13 20:42:57 -0400

    agregar ProductMapper para la conversión entre ProductCreateDTO y ProductResponseDTO

---
+++ src/main/java/com/sbravoc/products/management/mapper/ProductMapper.java
@@ -0,0 +1,26 @@
+package com.sbravoc.products.management.mapper;
+
+import com.sbravoc.products.management.dtos.ProductCreateDTO;
+import com.sbravoc.products.management.dtos.ProductResponseDTO;
+import com.sbravoc.products.management.entities.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+    public Product toEntity(ProductCreateDTO dto) {
+        Product product = new Product();
+        product.setName(dto.getName());
+        product.setPrice(dto.getPrice());
+        product.setDescription(dto.getDescription());
+        return product;
+    }
+
+    public ProductResponseDTO toResponseDTO(Product product) {
+        ProductResponseDTO dto = new ProductResponseDTO();
+        dto.setId(product.getId());
+        dto.setName(product.getName());
+        dto.setPrice(product.getPrice());
+        dto.setDescription(product.getDescription());
+        return dto;
+    }
+}
